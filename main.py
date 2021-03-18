048a98e8df13b7d930cc90a8f2b8133edbf23c0a
bryanleekw <bryanleekw@gmail.com>
2021-03-18 20:00:30 +0800

    20210318

---
+++ main.py
@@ -0,0 +1,53 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import pandas as pd
+# get weekday
+import calendar
+
+
+def print_func_name(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Function name is, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+def basic_feature_extract(df):
+    df_= df.copy()
+    # pickup
+    df_["pickup_date"] = pd.to_datetime(df_.lpep_pickup_datetime.apply(lambda x : x.split(" ")[0]))
+    df_["pickup_hour"] = df_.lpep_pickup_datetime.apply(lambda x : x.split(" ")[1].split(":")[0])
+    df_["pickup_year"] = df_.lpep_pickup_datetime.apply(lambda x : x.split(" ")[0].split("-")[0])
+    df_["pickup_month"] = df_.lpep_pickup_datetime.apply(lambda x : x.split(" ")[0].split("-")[1])
+    df_["pickup_weekday"] = df_.lpep_pickup_datetime.apply(lambda x :pd.to_datetime(x.split(" ")[0]).weekday())
+    # dropoff
+    # in case test data dont have dropoff_datetime feature
+    try:
+        df_["dropoff_date"] = pd.to_datetime(df_.lpep_dropoff_datetime.apply(lambda x : x.split(" ")[0]))
+        df_["dropoff_hour"] = df_.lpep_dropoff_datetime.apply(lambda x : x.split(" ")[1].split(":")[0])
+        df_["dropoff_year"] = df_.lpep_dropoff_datetime.apply(lambda x : x.split(" ")[0].split("-")[0])
+        df_["dropoff_month"] = df_.lpep_dropoff_datetime.apply(lambda x : x.split(" ")[0].split("-")[1])
+        df_["dropoff_weekday"] = df_.lpep_dropoff_datetime.apply(lambda x :pd.to_datetime(x.split(" ")[0]).weekday())
+    except:
+        pass
+    return df_
+
+
+def get_weekday(df):
+    list(calendar.day_name)
+    df_=df.copy()
+    df_['pickup_week_'] = pd.to_datetime(df_.lpep_pickup_datetime,coerce=True).dt.weekday
+    df_['pickup_weekday_'] = df_['pickup_week_'].apply(lambda x: calendar.day_name[x])
+    return df_
+
+# get trip duration
+def get_duration(df):
+    df_= df.copy()
+    df_['trip_duration_cal'] = pd.to_datetime(df_['dropoff_datetime']) - pd.to_datetime(df_['pickup_datetime'])
+    return df_
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_func_name('NYC Taxi')
+
